3c46083f5865a714247a9696df3eb978a21ff37c
Pavel Makhov <pmakhov@solutionsmedias360.com>
2021-02-27 20:45:30 -0500

    Allow transition tickets

--- init.lua
+++ init.lua
@@ -12,13 +12,18 @@ obj.indicator = nil
 obj.timer = nil
 obj.jira_host = nil
 obj.jql = 'assignee=currentuser()+AND+resolution=Unresolved&fields=id,assignee,summary,status'
-obj.login = nil
-obj.passowrd = nil
-
+obj.jira_menu = {}
+obj.icon_type = nil
 obj.iconPath = hs.spoons.resourcePath("icons")
 
+local auth_header
+
 local function show_warning(status, body)
-    hs.alert(string.format('Jira Spoon Error: received status: %s', status))
+    hs.notify.new(function() end, {
+        autoWithdraw = false,
+        title = 'Jira Spoon',
+        informativeText = string.format('Received status: %s\nbody:%s', status, string.sub(body, 1, 400))
+    }):send()
 end
 
 local user_icon = hs.styledtext.new(' ', { font = {name = 'feather', size = 12 }, color = {hex = '#8e8e8e'}})
@@ -30,23 +35,24 @@ end
 
 local function updateMenu()
     local jira_url = obj.jira_host .. '/rest/api/2/search?jql=' .. obj.jql
-    hs.http.asyncGet(jira_url, {Authorization = 'Basic ' .. hs.base64.encode(string.format('%s:%s', obj.login, obj.password))}, function(status, body)
+    hs.http.asyncGet(jira_url, {Authorization = auth_header}, function(status, body)
+        obj.jira_menu = {}
+
         if status ~=200 then
-            show_warning(status)
+            show_warning(status, body)
             return
         end
         
         local issues = hs.json.decode(body).issues
         obj.indicator:setTitle(#issues)
-        local jira_menu = {}
     
         table.sort(issues, function(left, right) return left.fields.status.name < right.fields.status.name end)
     
         local cur_status = ''
         for _, issue in ipairs(issues) do
             if cur_status ~= issue.fields.status.name then
-                table.insert(jira_menu, { title = '-'})
-                table.insert(jira_menu, { title = issue.fields.status.name, disabled = true})
+                table.insert(obj.jira_menu, { title = '-'})
+                table.insert(obj.jira_menu, { title = issue.fields.status.name, disabled = true})
                 cur_status = issue.fields.status.name
             end
             
@@ -57,34 +63,66 @@ local function updateMenu()
                 and styledText('Unassigned')
                 or styledText(issue.fields.assignee.displayName)
             
-                table.insert(jira_menu, {
+            local transitions_url = obj.jira_host .. '/rest/api/2/issue/' .. issue.key .. '/transitions'
+            
+            local transitions_submenu = {}
+            hs.http.asyncGet(transitions_url, {Authorization = auth_header}, function(status, body)
+
+                if status == 200 then
+                    local transitions = hs.json.decode(body).transitions
+                    for _, transition in ipairs(transitions) do
+                        local transition_payload = string.format([[{ "transition": { "id": "%s" } }]], transition.id)
+                        local header = {Authorization = auth_header}
+                        header['content-type'] = 'application/json'
+                        table.insert(transitions_submenu, {
+                            image = hs.image.imageFromURL(transition.to.iconUrl):setSize({w=16,h=16}),
+                            title = transition.name,
+                            fn = function() hs.http.asyncPost(transitions_url, transition_payload, header, function(status, body) 
+                                if status ~= 204 then
+                                    show_warning(status, body)
+                                end
+                                updateMenu() end) 
+                            end
+                        })
+                    end
+                end
+            end)
+
+            table.insert(obj.jira_menu, {
                 image = avatar,
                 title = hs.styledtext.new(' ' .. issue.fields.summary .. '\n')
-                .. ticket_icon .. styledText(issue.key .. '   ')
-                .. user_icon .. assignee_name,
+                    .. ticket_icon .. styledText(issue.key .. '   ')
+                    .. user_icon .. assignee_name,
+                menu = transitions_submenu,
                 fn = function() os.execute(string.format('open %s/browse/%s', obj.jira_host, issue.key)) end
             })
         end
         
-        table.insert(jira_menu, { title = '-'})
-        table.insert(jira_menu, { title = 'Refresh', fn = function() updateMenu() end})
-
-        obj.indicator:setMenu(jira_menu)
+        table.insert(obj.jira_menu, { title = '-'})
+        table.insert(obj.jira_menu, { title = 'Refresh', fn = function() updateMenu() end})
     end)
 end
 
+function obj:buildMenu()
+    local ret = {}
+    for _,v in ipairs(obj.jira_menu) do 
+        table.insert(ret, v)
+    end
+
+    return ret
+end
 
 function obj:init()
     self.indicator = hs.menubar.new()
     self.indicator:setIcon(hs.image.imageFromPath(obj.iconPath .. '/jira-mark-gradient-blue.png'):setSize({w=16,h=16}), true)
+    obj.indicator:setMenu(self.buildMenu)
 
     self.timer = hs.timer.new(300, updateMenu)
 end
 
 function obj:setup(args)
     self.jira_host = args.jira_host
-    self.login = args.login
-    self.password = args.password
+    auth_header = 'Basic ' .. hs.base64.encode(string.format('%s:%s', args.login, args.password))
     if args.jql ~= nil then obj.jql = hs.http.encodeForQuery(args.jql) .. '&fields=id,assignee,summary,status' end
 end
 
